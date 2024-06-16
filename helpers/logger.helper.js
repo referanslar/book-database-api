31686260d70d8bbd414f755ee32810aeecf2f14e
referanslar <147508935+referanslar@users.noreply.github.com>
2024-06-16 17:54:29 +0300

    feat: create `logger` with `winston`

---
+++ helpers/logger.helper.js
@@ -0,0 +1,37 @@
+import winston from "winston";
+
+import loggerConfig from "../config/logger.config.js";
+
+const fileTransports = {
+  level: loggerConfig.file.level,
+  filename: loggerConfig.file.filename,
+  handleExceptions: true,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+    winston.format.prettyPrint()
+  ),
+};
+
+const consoleTransports = {
+  level: loggerConfig.console.level,
+  handleExceptions: true,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, timestamp }) => {
+      const ts = timestamp.slice(0, 19).replace("T", " ");
+      return `${ts} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+};
+
+const logger = winston.createLogger({
+  levels: winston.config.npm.levels,
+  transports: [
+    new winston.transports.File(fileTransports),
+    new winston.transports.Console(consoleTransports),
+  ],
+  exitOnError: false,
+});
+
+export default logger;
