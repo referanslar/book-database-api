f439520743d81284d674d14d5f2514b6f36b8998
referanslar <147508935+referanslar@users.noreply.github.com>
2024-06-17 01:23:45 +0300

    feat: add `authors` routes to `index`

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,18 +3,13 @@ import express from "express";
 import indexController from "../controllers/index.controller.js";
 
 import authRotes from "./auth.routes.js";
+import authorsRoutes from "./authors.routes.js";
 import swaggerRoutes from "./swagger.routes.js";
-import authCheck from "../middleware/auth-check.middleware.js";
 
 const router = express.Router();
 
 router.get("/", indexController.welcomePage);
 
-router.get("/protected", authCheck, (req, res) => {
-  const userInfo = req.payload;
-  res.json(userInfo);
-});
-
-router.use("/auth", authRotes).use("/swagger", swaggerRoutes);
+router.use("/auth", authRotes).use("/authors", authorsRoutes).use("/swagger", swaggerRoutes);
 
 export default router;
