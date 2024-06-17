4872890744925689d800f5da30da596bd27b6c18
referanslar <147508935+referanslar@users.noreply.github.com>
2024-06-18 00:28:38 +0300

    feat: create `admin check` functionality to `middleware`

--- middleware/admin-check.middleware.js
+++ middleware/admin-check.middleware.js
@@ -1,10 +1,23 @@
+import createError from "http-errors";
 import logger from "../helpers/logger.helper.js";
 
-const adminCheck = (req, res, next) => {
-  logger.debug(`Admin Check Middleware: ${req.payload.aud}`);
-  logger.debug(`Admin Check Middleware: ${req.originalUrl}`);
+import usersService from "../services/users.service.js";
 
-  next();
+const adminCheck = async (req, res, next) => {
+  try {
+    const userID = req.payload.aud;
+    const user = await usersService.getUserByID(userID);
+
+    if (user.role !== "admin") {
+      return next(createError.Unauthorized("You are not authorized to perform this action."));
+    }
+
+    next();
+  } catch (error) {
+    return next(
+      createError.InternalServerError("An error occurred while verifying your authorization.")
+    );
+  }
 };
 
 export default adminCheck;
